c81a0e538ca86ce58e96ca5c6e3879dfa5741516
Milo Fultz <milopfultz@gmail.com>
2020-07-22 22:36:47 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The Awfulizer
+---
+
+I wanted to see what I could do with Firefox extensions and some basic Javascript, CSS, and HTML knowledge. So here is a browser add on that will just cycle through random colors of backgrounds and text about 6 times a second. ¯\_(ツ)_/¯
---
+++ css_random.js
@@ -0,0 +1,40 @@
+/* 
+
+CSS Color Randomizer
+
+I want to make a Firefox extension that will just randomize the CSS color values that are available on a given webpage. I want crazy colors and wild formatting.
+
+For now, I will limit my scope to changing the font color and the background color to random hex values.
+
+Problem: Webpages are boring
+Solution: Change all the text and background colors randomly when you click the button
+
+Look at the CSS
+Take down notes of what elements exist
+Circle the ones we want (background, color)
+
+For each of the desired elements
+Make random number that will fit in 4096 and output in hex
+Replace the given color code with new 3 digit hex
+
+Reload the webpage
+
+*/
+
+function getRandomHex(max) {
+	var intNumber = Math.floor(Math.random() * Math.floor(max));
+	return '#' + intNumber.toString(16);
+}
+
+function changeAllColors() {
+
+	var tags = document.getElementsByTagName("*");
+
+	for (var i=0, max=tags.length; i < max; i++) {
+		tags[i].style.backgroundColor = getRandomHex(4096);
+		tags[i].style.color = getRandomHex(4096);
+	}
+	setTimeout(changeAllColors, 150)
+}
+
+setTimeout(changeAllColors, 150)
